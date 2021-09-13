3409f2efa8dcba0701b9d11423a5d260ccdfc480
taka <shimakuratakanao@gmail.com>
2021-09-13 20:05:19 +0900

    リファクタリング

--- matome2.c
+++ matome2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i, intarval_num=1000;
+    double trapezoid_rule_func(), trap_answer;
+    trap_answer = trapezoid_rule_func();
+    printf("The integral = %15.7e\n", trap_answer);
+}
+
+double trapezoid_rule_func()
+{
+   int i, intarval_num=1000;
     double x, sum_y, dx, sum, trapezoid_size, to=1.0, start_from=0.0;
     double culc_func(double);
     dx = (to - start_from)/intarval_num;
@@ -12,9 +19,10 @@ int main()
         sum_y = (culc_func(x) + culc_func(x + dx));
         trapezoid_size = sum_y*dx*0.5;
         sum += trapezoid_size;
-    }
-    printf("The integral = %15.7e\n", sum);
+    } 
+    return sum;
 }
+
 double culc_func(double x)
 {
     double y;

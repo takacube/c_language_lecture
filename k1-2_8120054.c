7160fb5592a579ea7b13a89ce3f7ce47eed180bf
taka <shimakuratakanao@gmail.com>
2021-09-29 13:26:06 +0900

    delete srand

--- k1-2_8120054.c
+++ k1-2_8120054.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <math.h>
 double spread_num(times) {
     int count;
     int count_in = 0;
-    srand((unsigned int)time(NULL));
     for(count=1;count<=times;count++) {
         double x = (double)rand()/RAND_MAX;
         double y = (double)rand()/RAND_MAX;

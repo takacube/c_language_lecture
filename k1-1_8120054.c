53b0febb9566682c830be216fcc100361d7013e9
taka <shimakuratakanao@gmail.com>
2021-10-03 23:11:21 +0900

    delete main which is not needed anymore

--- k1-1_8120054.c
+++ k1-1_8120054.c
@@ -13,24 +13,6 @@ double spread_num(times) {
     }
     return (double)count_in/times*4;
 }
-/*
-int main(void) {
-    double v_10 = spread_num(10);
-    printf("%f : %f\n", v_10, fabs(v_10-M_PI));
-    double v_100 = spread_num(100);
-    printf("%f : %f\n", v_100, fabs(v_100-M_PI));
-    double v_1000 = spread_num(1000);
-    printf("%f : %f\n", v_1000, fabs(v_1000-M_PI));
-    double v_10000 = spread_num(10000);
-    printf("%f : %f\n", v_10000, fabs(v_10000-M_PI));
-    double v_100000 = spread_num(100000);
-    printf("%f : %f\n", v_100000, fabs(v_100000-M_PI));
-    double v_1000000 = spread_num(1000000);
-    printf("%f : %f\n", v_1000000, fabs(v_1000000-M_PI));
-    double v_10000000 = spread_num(10000000);
-    printf("%f : %f\n", v_10000000, fabs(v_10000000-M_PI));
-}
-*/
 
 int main(void) {
     int i;

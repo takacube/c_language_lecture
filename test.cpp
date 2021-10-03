70cd7f05aedb816135029380216d7b6c4f8ce8f4
taka <shimakuratakanao@gmail.com>
2021-10-04 01:44:25 +0900

    just test to improve the rate of most used lanhuage

--- test.cpp
+++ test.cpp
@@ -3,3 +3,88 @@ int main() {
     int first_num = 90;
     printf("%d", first_num);
 }
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}
+#include <stdio.h>
+int main() {
+    int first_num = 90;
+    printf("%d", first_num);
+}

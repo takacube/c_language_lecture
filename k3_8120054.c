7327bfc844f8f58ab2c89a1a40c766ff69237198
taka <shimakuratakanao@gmail.com>
2021-10-13 10:52:22 +0900

    fix

--- k3_8120054.c
+++ k3_8120054.c
@@ -9,7 +9,7 @@ int main() {
     int n, char_num;
     for (n=1; n< 25; n++) {
         char text[] = "MCI AWGG 100% CT HVS GVCHG MCI RCB'H HOYS.";
-        char extra = '[';
+        char extra = '['; //Zの次の文字(91)に対応する.
         for (char_num=0; char_num<42; char_num++) {
             if(65 <= text[char_num] && text[char_num] <= 90){
                 text[char_num] -= n;
@@ -20,6 +20,6 @@ int main() {
             
         }
         printf("%s", text);
-        printf("\n\n");
+        printf("\n");
     }
 }
